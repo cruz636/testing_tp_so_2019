135c849409294b20a0379ef1fd4b38f5c475cf8b
juan_cruz <juanbarcruz96@gmail.com>
2019-11-08 20:41:47 -0300

    A

---
+++ pruebas.c
@@ -0,0 +1,15 @@
+#include pruebas.h
+
+/*
+prueba de funciones de libmuse
+
+*/
+
+
+int main()
+{
+
+
+  //muse_get(void* dst, uint32_t src, size_t n);
+  return 0;
+}
---
+++ pruebas.h
@@ -0,0 +1,13 @@
+#ifndef PRUEBAS_H
+#define PRUEBAS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "libmuse.h"
+#include "libmuse.c"
+
+
+
+#endif
